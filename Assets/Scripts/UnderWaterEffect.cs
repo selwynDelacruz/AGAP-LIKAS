49d7dd403a0cf2fc175a67b4035d77534c765262
John Kenneth Galapia <johnkennethgalapia@gmail.com>
2025-03-27 15:43:17 +0800

    flood changes
    depth of field update

---
+++ Assets/Scripts/UnderWaterEffect.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class UnderWaterEffect : MonoBehaviour
+{
+    public Transform waterPlane; // Assign the water plane
+    public Volume postProcessVolume; // Assign the Global Volume
+    private DepthOfField depthOfField;
+    private bool isUnderwater = false;
+
+    void Start()
+    {
+        // Get the Depth of Field effect from the Volume
+        if (postProcessVolume.profile.TryGet(out depthOfField))
+        {
+            depthOfField.active = false; // Start with blur disabled
+        }
+    }
+
+    void Update()
+    {
+        if (transform.position.y < waterPlane.position.y && !isUnderwater)
+        {
+            EnterUnderwater();
+        }
+        else if (transform.position.y >= waterPlane.position.y && isUnderwater)
+        {
+            ExitUnderwater();
+        }
+    }
+
+    void EnterUnderwater()
+    {
+        isUnderwater = true;
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = new Color(0.1f, 0.4f, 0.7f, 1f);
+        RenderSettings.fogDensity = 0.05f;
+
+        if (depthOfField != null)
+        {
+            depthOfField.active = true; // Enable blur effect
+            depthOfField.focusDistance.value = 0.5f;
+            depthOfField.aperture.value = 8;
+            depthOfField.focalLength.value = 50;
+        }
+    }
+
+    void ExitUnderwater()
+    {
+        isUnderwater = false;
+        RenderSettings.fog = false;
+
+        if (depthOfField != null)
+        {
+            depthOfField.active = false; // Disable blur effect
+        }
+    }
+}
