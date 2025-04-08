81fa5a9c833e712ec828c1d247fb83331e116496
Ashley <134979936+lemonexxe@users.noreply.github.com>
2025-04-08 14:33:54 +0800

    flood update: stamina bar
    stamina bar UI (2D and slider) + player script (logic, wasd movement)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxStamina = 100;
+    public float currentStamina;
+
+    public StaminaBar staminaBar;
+
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.SetMaxStamina(maxStamina);
+    }
+
+    void Update()
+    {
+        // Example of stamina usage
+        if (Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.D))
+        {
+            UseStamina(0.1f);
+        }
+        // Example of stamina regeneration
+        if (!Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.D))
+        {
+            RegenerateStamina(0.1f);
+        }
+    }
+
+    public void UseStamina(float amount)
+    {
+        if (currentStamina >= amount)
+        {
+            currentStamina -= amount;
+            Debug.Log("Used " + amount + " stamina. Current stamina: " + currentStamina);
+        }
+        else
+        {
+            Debug.Log("Not enough stamina!");
+        }
+
+        staminaBar.SetStamina(currentStamina);
+    }
+
+    public void RegenerateStamina(float amount)
+    {
+        if (currentStamina + amount <= maxStamina)
+        {
+            currentStamina += amount;
+            Debug.Log("Regenerated " + amount + " stamina. Current stamina: " + currentStamina);
+        }
+        else
+        {
+            currentStamina = maxStamina;
+            Debug.Log("Stamina is full!");
+        }
+        staminaBar.SetStamina(currentStamina);
+    }
+}
