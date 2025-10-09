4382ed6e9238c0f88163f8e90ba13d7d49fd2486
Ashley <134979936+lemonexxe@users.noreply.github.com>
2025-10-09 13:30:41 +0800

    map prefabs
    map prefabs (6) + map generator code (to edit still)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,16 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("All map prefabs")] 
+    public GameObject[] mapPrefabs;   // Holds 6 map prefabs
+
+    [Header("Number of maps to spawn")]
+    public int mapsToSpawn = 4;
+
+    [Header("Offset between maps")]
+    public Vector3 mapOffset = new Vector3(0, 0, 100);  // distance between spawned chunks
+
+    private List<int> chosenIndexes = new List<int>();
+
     void Start()
     {
-        
+        GenerateMap();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateMap()
     {
-        
+        // Make sure we don’t pick the same chunk twice
+        List<int> availableIndexes = new List<int>();
+        for (int i = 0; i < mapPrefabs.Length; i++)
+        {
+            availableIndexes.Add(i);
+        }
+
+        for (int i = 0; i < mapsToSpawn; i++)
+        {
+            // Pick a random index from the available pool
+            int randomIndex = Random.Range(0, availableIndexes.Count);
+            int chosenIndex = availableIndexes[randomIndex];
+
+            // Instantiate the chunk at the correct position
+            Vector3 spawnPosition = i * mapOffset;
+            Instantiate(mapPrefabs[chosenIndex], spawnPosition, Quaternion.identity);
+
+            // Remove it from pool to avoid duplicates
+            availableIndexes.RemoveAt(randomIndex);
+            chosenIndexes.Add(chosenIndex);
+        }
     }
 }
